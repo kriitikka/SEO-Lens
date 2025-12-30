5116f438866c713f24c39a45ef2337573f87368d
kriitikka <161369966+kriitikka@users.noreply.github.com>
2025-12-30 15:04:22 +0530

    Update serp_scraper.py

--- backend/serp_scraper.py
+++ backend/serp_scraper.py
@@ -1,20 +1,19 @@
 import requests
 import os
+from dotenv import load_dotenv
 import json
 from pathlib import Path
-import streamlit as st
 
-serp_key = st.secrets["SERPAPI_KEY"]
-gemini_key = st.secrets["GEMINI_API_KEY"]
+load_dotenv()
+
+serp_key = os.getenv("SERPAPI_KEY")  # Returns the key
+gemini_key = os.getenv("GEMINI_API_KEY")
 print(serp_key)
 print(gemini_key)
 
 def scrape_serp(keyword: str, num_results: int = 10) -> list[dict]:
-    """
-    Fetches top Google search results for a keyword using SerpAPI.
-    Returns a list of dictionaries with 'title', 'link', and 'snippet'.
-    """
-    api_key = st.secrets["SERPAPI_KEY"]
+    
+    api_key = os.getenv("SERPAPI_KEY")
     if not api_key:
         raise ValueError("SERPAPI_KEY not found in .env")
 
@@ -22,13 +21,21 @@ def scrape_serp(keyword: str, num_results: int = 10) -> list[dict]:
         "q": keyword,
         "api_key": api_key,
         "num": num_results,
-        "engine": "google"  # Use 'google_images' or 'google_news' for other types
+        "engine": "google", 
+        "hl" : "en",
+        "gl": "us"
     }
 
     try:
         response = requests.get("https://serpapi.com/search", params=params)
-        response.raise_for_status()  # Raises HTTPError for bad responses
-        return response.json().get("organic_results", [])
+        response.raise_for_status()  
+        data = response.json()
+        
+        return {
+            "organic": data.get("organic_results", []),
+            "answer_box": data.get("answer_box", {}),
+            "paa": data.get("related_questions", [])  # People Also Ask
+        }
     except requests.exceptions.RequestException as e:
         print(f"Error fetching SERP data: {e}")
         return []
